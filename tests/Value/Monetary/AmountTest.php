cc0b6c15518f6042db993aefd261a4ff438d8bc1
Fred-Jan van der Eijken <fred-jan@vandereijken.nl>
2018-09-13 11:58:55 +0200

    Amount VO now actually allows integers as input per PHP 7.2

--- tests/Value/Monetary/AmountTest.php
+++ tests/Value/Monetary/AmountTest.php
@@ -7,10 +7,46 @@ use MyOnlineStore\Common\Domain\Value\Monetary\Amount;
 
 final class AmountTest extends \PHPUnit\Framework\TestCase
 {
-    public function testInvalidConstruction()
+    public function invalidInputProvider()
+    {
+        yield ['1.23'];
+        yield [1.23];
+        yield ['a'];
+    }
+
+    /**
+     * @dataProvider invalidInputProvider
+     *
+     * @param mixed $input
+     */
+    public function testInvalidInput($input)
     {
         $this->expectException(\InvalidArgumentException::class);
 
-        new Amount('1.23');
+        new Amount($input);
+    }
+
+    public function validInputProvider()
+    {
+        yield [123];
+        yield ['123'];
+    }
+
+    /**
+     * @dataProvider validInputProvider
+     *
+     * @param mixed $input
+     */
+    public function testValidInput($input)
+    {
+        self::assertInstanceOf(Amount::class, new Amount($input));
+    }
+
+    public function testAsZero()
+    {
+        $zeroAmount = Amount::asZero();
+
+        self::assertTrue($zeroAmount->isZero());
+        self::assertSame('0', (string) $zeroAmount);
     }
 }
