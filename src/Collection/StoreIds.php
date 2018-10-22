5d702dd96e4c0bbb1efc40145b5d6e714f0faed7
Fred-Jan van der Eijken <fred-jan@vandereijken.nl>
2018-10-22 16:00:21 +0200

    StoreIds collection added

---
+++ src/Collection/StoreIds.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace MyOnlineStore\Common\Domain\Collection;
+
+use MyOnlineStore\Common\Domain\Value\StoreId;
+
+final class StoreIds extends ImmutableCollection implements StoreIdsInterface
+{
+    use StringCollectionTrait;
+
+    public function __construct(array $entries = [])
+    {
+        parent::__construct(
+            array_map(
+                function ($entry) {
+                    return ($entry instanceof StoreId) ? $entry : new StoreId($entry);
+                },
+                $entries
+            )
+        );
+    }
+
+    public function contains($element)
+    {
+        return in_array($element, $this->getArrayCopy(), false);
+    }
+}
---
+++ src/Collection/StoreIdsInterface.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace MyOnlineStore\Common\Domain\Collection;
+
+use MyOnlineStore\Common\Domain\Value\StoreId;
+
+/**
+ * @method StoreId[] getIterator()
+ */
+interface StoreIdsInterface extends ImmutableCollectionInterface
+{
+}
