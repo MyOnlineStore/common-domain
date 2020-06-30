14548879ecf0b6f14e51aba7e779758fd51793ba
Frank Verhoeven <frank@myonlinestore.com>
2020-06-30 14:28:54 +0200

    Minor performance improvement for Locale::equals (#26)

---
+++ tests/benchmarks/Value/LocaleBench.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace MyOnlineStore\Common\Domain\Benchmark\Value;
+
+use MyOnlineStore\Common\Domain\Value\Locale;
+use PhpBench\Benchmark\Metadata\Annotations\BeforeMethods;
+use PhpBench\Benchmark\Metadata\Annotations\Iterations;
+use PhpBench\Benchmark\Metadata\Annotations\Revs;
+
+/**
+ * @BeforeMethods({"init"})
+ */
+final class LocaleBench
+{
+    /** @var Locale */
+    private $locale;
+
+    /** @var Locale */
+    private $comparator;
+
+    public function init(): void
+    {
+        $this->locale = Locale::fromString('nl_NL');
+        $this->comparator = Locale::fromString('en_GB');
+    }
+
+    /**
+     * @Revs(10000)
+     * @Iterations(5)
+     */
+    public function benchEquals(): void
+    {
+        $this->locale->equals($this->comparator);
+    }
+}
