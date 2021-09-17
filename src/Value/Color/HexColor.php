dc199a2f1d46412dd5a8abf8afbb2f4985ef1eee
Jordy Kiesebrink <jordy@myonlinestore.com>
2021-09-17 13:43:26 +0200

    feat(BIER-3496): Add `HexColor` VO

---
+++ src/Value/Color/HexColor.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types=1);
+
+namespace MyOnlineStore\Common\Domain\Value\Color;
+
+use MyOnlineStore\Common\Domain\Exception\Color\InvalidHexColor;
+
+/**
+ * @final
+ * @psalm-immutable
+ */
+class HexColor
+{
+    private function __construct(
+        private string $value
+    ) {
+    }
+
+    /**
+     * @throws \InvalidArgumentException
+     */
+    public static function fromString(string $value): self
+    {
+        if (!self::isValidHexColor($value)) {
+            throw InvalidHexColor::withHexColor($value);
+        }
+
+        if (1 === \preg_match('/#([a-f0-9]{6})\b/i', $value)) {
+            $value = \strtoupper($value);
+        }
+
+        if (1 === \preg_match('/#([a-f0-9]{3})\b/i', $value)) {
+            $value = \strtoupper(
+                \sprintf('#%s%s%s%s%s%s', $value[1], $value[1], $value[2], $value[2], $value[3], $value[3])
+            );
+        }
+
+        return new self($value);
+    }
+
+    public function toString(): string
+    {
+        return $this->value;
+    }
+
+    private static function isValidHexColor(string $value): bool
+    {
+        if (1 === \preg_match('/#([a-f0-9]{6})\b/i', $value)) {
+            return true;
+        }
+
+        return 1 === \preg_match('/#([a-f0-9]{3})\b/i', $value);
+    }
+}
